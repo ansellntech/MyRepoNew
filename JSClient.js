44061dbdcd23131ccc23cf94fe7760523fb4123e
anselmoarantes <anselmoarantes@hotmail.com.br>
2015-10-18 16:06:20 -0200

    Update JSClient.js

--- JSClient.js
+++ JSClient.js
@@ -1,1 +1,10 @@
 // Script To Create a Cliente Env For My Data Retrieving
+function LoadJS(target){
+  var script = document.createElement("script");
+  script.src = target;
+  try{
+    document.body.addChild(script);
+  }catch(Exception e){
+    console.log(e);
+  }
+}
